f1da7cafba904914a3b271d30105c865961d0de8
Lucas S Silva <143051956+Lucas-7s@users.noreply.github.com>
2025-06-27 17:43:06 -0300

    update 0.1

--- src/com/lucas/javatraining/encapsulationBank/BankAccont.java
+++ src/com/lucas/javatraining/encapsulationBank/BankAccont.java
@@ -1,5 +1,39 @@
 package com.lucas.javatraining.encapsulationBank;
 
+import java.util.Scanner;
+
 public class BankAccont {
 
+	public static double abrirConta(String tipoConta) {
+		if (tipoConta == "conta corrente") {
+			return 50;
+		} else if (tipoConta == "conta poupanca") {
+			return 150;	
+		} else {
+			return 0;
+		}
+		
+	}
+	
+	public static void fecharConta() {
+		
+	}
+	
+	public static void depositar() {
+		
+	}
+	
+	public static void sacar() {
+		
+	}
+	
+	public static void pagarMensalidade() {
+		
+	}
+	
+	public final static void clearConsole(){
+		
+		for (int i = 0; i < 50; i++) System.out.println();
+	}
 }
+
--- src/com/lucas/javatraining/encapsulationBank/Main.java
+++ src/com/lucas/javatraining/encapsulationBank/Main.java
@@ -1,10 +1,76 @@
 package com.lucas.javatraining.encapsulationBank;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int spacingSize = 55;
+		String pularLinha =  "\n" + "=".repeat(spacingSize) + "\n";
+		
+		Interface.Intro(spacingSize);
+		Interface.Menu1();
+		System.out.println(pularLinha);
+		Interface.Menu2(0);
+    }
+	
+	//=================================================================
+	
+	public class Interface {
 
+		public static void Intro(int spacingSize) {
+			//declaração de variáveis
+	        String title, spacing, intro;
+	        
+	        //Interface no Console - Boas Vindas
+	        title = "Bem vindo ao Banco Java";
+	        spacing = " ".repeat((spacingSize - title.length())/2);
+	        System.out.print("=".repeat(spacingSize) + "\n"
+	                        + spacing + title.toUpperCase() + spacing + "\n"
+	                        + "=".repeat(spacingSize) + "\n");
+		}
+		
+		//=================================================================
+		
+	    public static double Menu1() {
+	    	//declaração de variáveis
+	    	String escolha;
+	    	double saldoMenu1 = 0;
+	        
+	        Scanner teclado = new Scanner(System.in);
+
+
+	        //Login ou criar conta
+	        System.out.print("[1] Acessar\n"
+	                                        + "[2] Criar uma Conta Corrente (Premio: R$ 50,00)\n"
+	                                        + "[3] Criar uma Conta Poupança (Premio: R$ 150,00)\n"
+	                                        + "Qual a sua escolha?: ");
+
+	        do {
+	            escolha = teclado.nextLine();
+	            if(!escolha.equals("1") && !escolha.equals("2") && !escolha.equals("3")) {
+	                System.out.print("Valor incorreto, esperado um número de 1 à 3: ");
+	            }
+	        } while(!escolha.equals("1") && !escolha.equals("2") && !escolha.equals("3"));
+
+	        switch (escolha) {
+	        case "1":
+	            System.out.println("OK");
+	        case "2":
+	        	saldoMenu1 = BankAccont.abrirConta("conta corrente");
+			case "3":
+				saldoMenu1 = BankAccont.abrirConta("conta poupanca");
+	        }
+
+	        teclado.close();
+	        return saldoMenu1;
+	    }
+	    
+	    //=================================================================
+	    
+	    public static void Menu2(double saldoMenu2) {
+            System.out.printf("Obrigado por abrir uma Conta Poupança conosco \nSaldo atual: R$ %.2f", saldoMenu2);
+	    }
 	}
 
 }
