655fd30126f61da3f2301d0ab3665070ae9ec6cc
Lucas S Silva <143051956+Lucas-7s@users.noreply.github.com>
2025-07-03 11:34:51 -0300

    update

---
+++ src/com/lucas/javatraining/Ex03IfElseNumerosIguais.java
@@ -0,0 +1,48 @@
+/*
+ * Programa para treinar condicional composta (if e else)
+ * Verifica as condições de dois números
+ */
+
+package com.lucas.javatraining;
+
+import java.util.Scanner;
+
+public class Ex03IfElseNumerosIguais {
+
+	public static void main(String[] args) {
+		
+		//permite entrada de dados
+		Scanner teclado = new Scanner(System.in);
+		
+		//declaração de variáveis
+		//intro
+		String titulo = "Programa que verifica as condições de dois números";
+		//entrada
+		int num1,num2;
+		
+		//intro
+		System.out.println("=".repeat(55));
+		System.out.println(" ".repeat((55 - titulo.length())/2) + titulo.toUpperCase() + " ".repeat((55 - titulo.length())/2));
+		System.out.println("=".repeat(55) + "\n");
+		
+		//entrada
+		System.out.print("Digite o primeiro número: ");
+		num1 = teclado.nextInt();
+		
+		System.out.print("Digite o segundo número: ");
+		num2 = teclado.nextInt();
+		
+		//saida
+		if(num1 > num2) {
+			System.out.println("\n" + "O número " + num1 + " é maior que " + num2);
+		} else if (num1 == num2) {
+			System.out.println("\n" + "O número " + num1 + " é igual a " + num2);
+		} else {
+			System.out.println("\n" + "O número " + num1 + " é menor que " + num2);
+		}
+
+		System.out.print("\n" + "=".repeat(55));
+		teclado.close();
+	}
+
+}
