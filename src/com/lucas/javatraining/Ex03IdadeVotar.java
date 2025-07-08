6a0979daf1228388f44dc8fe7bb253fe8b9b3cda
Lucas S Silva <143051956+Lucas-7s@users.noreply.github.com>
2025-07-08 00:12:19 -0300

    update

---
+++ src/com/lucas/javatraining/Ex03IdadeVotar.java
@@ -0,0 +1,44 @@
+/*O QUE O PROGRAMA FAZ:
+ * Programa para treinar Estrutura Condicional Encadeada (if, else if, else)
+ * Programa que analisa idade obrigatoriedade de votos
+ */
+
+package com.lucas.javatraining;
+
+import java.util.Scanner;
+
+public class Ex03IdadeVotar {
+
+	public static void main(String[] args) {
+		
+		Scanner teclado = new Scanner(System.in);
+		
+		//Declaração de variáveis
+		String titulo = "Programa que verifica idade Minima para votar";
+		int ano, idade;
+		int anoAtual = 2025;
+		
+		//Intro
+		System.out.println("=".repeat(55));
+		System.out.println(" ".repeat((55 - titulo.length())/2) + titulo.toUpperCase());
+		System.out.println("=".repeat(55) + "\n");
+		
+		System.out.print("Qual o ano de seu nascimento?: ");
+		ano = teclado.nextInt();
+		
+		idade = anoAtual - ano;
+		
+		System.out.print("\n" + "Sua idade é " + idade);
+		
+		if(idade < 16) {
+			System.out.println(", Você não vota.");
+		} else if (idade < 18 || idade > 70) {
+			System.out.println(", Você não é obrigado a votar.");
+		} else {
+			System.out.println(", Você é obrigado a votar.");
+		}
+
+		System.out.println("\n" + "=".repeat(55));
+		teclado.close();
+	}
+}
