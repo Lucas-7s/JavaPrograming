630ae2394455c92ee8753642ad5211484a812812
Lucas S Silva <143051956+Lucas-7s@users.noreply.github.com>
2025-07-14 00:15:24 -0300

    update

--- src/com/lucas/javatraining/Ex05_While.java
+++ src/com/lucas/javatraining/Ex05_While.java
@@ -1,6 +1,6 @@
 /* O QUE O PROGRAMA FAZ:
  * Programa para treinar while
- * Conta números pulando números específicos
+ * Conta números até 20
  */
 
 package com.lucas.javatraining;
@@ -11,29 +11,29 @@ public class Ex05_While {
 
 		//declaração de variáveis
 		int cc;
-		String titulo = "Programa que Conta números pulando números específicos";
+		String titulo = "Programa que Conta Numero de 1 a 50";
 		
 		//intro
 		System.out.println("=".repeat(70));
 		System.out.println(" ".repeat((70 - titulo.length())/2) + titulo.toUpperCase());
-		System.out.println("=".repeat(70));
+		System.out.println("=".repeat(70) + "\n");
 		
 		cc = 0;
 		
-		while (cc<= 12) {
+		while (cc <= 50) {
 			cc ++;
 			
 			if(cc == 2 || cc == 4) {
 				continue;
 			}
 			
-			if(cc == 9) {
+			if(cc == 21) {
 				break;
 			}
-			System.out.println(cc);
+			System.out.print(cc + ", ");
 		}
 		
-		System.out.println("\n" + "=".repeat(70));
+		System.out.println("\n" + "\n" + "=".repeat(70));
 
 	}
 
