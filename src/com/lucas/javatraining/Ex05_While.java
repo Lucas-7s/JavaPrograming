e208b11794535c61eeb9c68c7a7dbd063a875b88
Lucas S Silva <143051956+Lucas-7s@users.noreply.github.com>
2025-07-18 00:54:24 -0300

    Update Ex05_While

--- src/com/lucas/javatraining/Ex05_While.java
+++ src/com/lucas/javatraining/Ex05_While.java
@@ -23,14 +23,21 @@ public class Ex05_While {
 		while (cc <= 50) {
 			cc ++;
 			
+			//condição, pula a contagem do 2 e 4
 			if(cc == 2 || cc == 4) {
 				continue;
 			}
 			
-			if(cc == 21) {
+			//encerra antes da hora, no 20
+			if(cc == 20) {
+				System.out.print(cc + ".");
 				break;
 			}
-			System.out.print(cc + ", ");
+			
+			//saida
+			System.out.print(cc);
+			//if simplificado
+			if(cc >= 1)	System.out.print(", ");
 		}
 		
 		System.out.println("\n" + "\n" + "=".repeat(70));
